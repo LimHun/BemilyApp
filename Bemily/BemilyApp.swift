329ddb57de9b5669cc0c063b1c0bca436bc0bf90
LimHun <dlagnskkld3@gmail.com>
2023-01-02 23:51:03 +0900

    Initial Commit

---
+++ Bemily/BemilyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BemilyApp.swift
+//  Bemily
+//
+//  Created by 임훈 on 2023/01/02.
+//
+
+import SwiftUI
+
+@main
+struct BemilyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
